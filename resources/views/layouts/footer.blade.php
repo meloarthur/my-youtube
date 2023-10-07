3fdcc1cbc12d42d643ad6833d9aef09d8e16ec26
Arthur Cáceres Melo <arthurcaceres@gmail.com>
2023-10-07 11:51:42 -0300

    criando rotas e novos arquivos

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,85 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+    .rodape {
+        width: 100%;
+    }
+
+    .redes-sociais, .area-contato {
+        width: 50%;
+        border-top:solid 1px #ccc;
+        float: left;
+        text-align: center;
+        background-color: #f8f8f8;
+        height: 250px;
+    }
+
+    .redes-sociais, .area-contato, p, span {
+        color: #333333;
+        font-size: 16px;
+    }
+
+    .redes-sociais img {
+        margin: 0px 15px 0px 15px;
+    }
+
+    .fa {
+        padding: 20px;
+        font-size: 30px;
+        width: 30px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 50%;
+    }
+
+    .fa:hover {
+        opacity: 0.7;
+    }
+
+    .fa-github {
+        background: #000;
+        color: white;
+    }
+
+    .fa-twitter {
+        background: #55ACEE;
+        color: white;
+    }
+
+    .fa-linkedin {
+        background: #3B5998;
+        color: white;
+    }
+
+    .mapouter {
+        /* position:relative;
+        text-align:right; */
+        margin: auto;
+        height:300px;
+        width:300px;
+    }
+
+    .gmap_canvas {
+        overflow:hidden;
+        background:none!important;
+        height:300px;
+        width:300px;
+    }
+</style>
+
+<div class="rodape">
+    <div class="redes-sociais">
+        <h2>Redes sociais</h2>
+        <a href="https://www.github.com/meloarthur/" class="fa fa-github" target=”_blank”></a>
+        <a href="https://www.linkedin.com/in/arthur-caceres-melo/" class="fa fa-linkedin" target=”_blank”></a>
+        <a href="https://twitter.com/devarthurdev" class="fa fa-twitter" target=”_blank”></a>
+    </div>
+    <div class="area-contato">
+        <h2>Contato</h2>
+        <span>Copyright 2023 - Arthur Cáceres Melo</span>
+        <br>
+        <span>Desenvolvedor de Software</span>
+        <br>
+        <span>(62) 99396-1008</span>
+    </div>
+</div>
---
+++ resources/views/site/videos.blade.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Sistema de Jogos - Jogos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+
+    <body>
+        <header>
+            @component('layouts.header')
+            @endcomponent
+        </header>
+
+        <div class="conteudo-pagina">
+            <div class="titulo-pagina">
+                <h1>Vídeos</h1>
+            </div>
+            
+            <a href="/jogos/cadastro" class="btn btn-primary add">
+                <button class="add" type="submit">Adicionar vídeo</button>
+            </a>
+
+            <div class="informacao-pagina">
+                <div class="table-responsive text-center">
+                    <table class="table table-hover" id="tabela">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Nome</th>
+                                <th class="text-center">Duração</th>
+                                <th class="text-center">Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Vídeo exemplo 1</td>
+                                <td>3:18</td>
+                                <td class="table-buttons">
+                                    <a href="/jogos/atualizar">
+                                        <button class="table-btn edit" type="submit"></button>
+                                    </a>
+                                    <form method="POST" action="/jogos/excluir">
+                                        @csrf
+                                        <button type="submit" class="table-btn delete"></button>
+                                    </form>
+                                </td>                
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <footer>
+            @component('layouts.footer')
+            @endcomponent
+        </footer>
+    </body>
+
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        h1 {
+            color: #ffffff;
+            font-size: 28px;
+        }
+
+        h2 {
+            color: #333333;
+            font-size: 22px;
+        }
+
+        input, select, textarea, button {
+            width: 100%;
+            padding: 10px 15px;
+            margin: 10px 0px 10px 0px;
+            box-sizing: border-box;
+            border-radius: 3px;
+            background-color: transparent;
+            color: #333;
+        }
+
+        button {
+            background-color: #7ab829;
+            cursor: pointer;
+            color: #fff;
+        }
+
+        button:hover {
+            background-color: #6ea22c;
+        }
+
+        ::placeholder {
+            color: #333333;
+            opacity: 1;
+        }
+
+        :-ms-input-placeholder {
+            color: #333333;
+        }
+
+        ::-ms-input-placeholder {
+            color: #333333;
+        }
+
+        .conteudo-pagina {
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            margin-bottom: 50px;
+        }
+
+        .titulo-pagina {
+            padding: 100px 0px 60px 0px;
+            background-color: #2a9ee2;
+            text-align: center;
+        }
+
+        .informacao-pagina {
+            width: 100%;
+            border: 2px rgba(0,0,0,.05) solid;
+
+        }
+
+        .informacao-pagina p{
+            color: #333;
+        }
+
+        th {
+            width: 25%;
+            font-size: 18px;
+            padding-right:20px;
+        }
+
+        th:first-child {
+            padding-left:20px;
+            padding-right:0;
+        }
+
+        td {
+            padding-right:20px;
+        }
+
+        td:first-child {
+            padding-left:20px;
+            padding-right:0;
+        }
+
+        .table-buttons {
+            display: flex;
+            gap: 0.5rem;
+            padding: 0.5rem 0rem;
+        }
+
+        .table-btn {
+            width: 45px;
+            height: 30px;
+            border-radius: 10px;
+        }
+
+        .add {
+            height: 53px;
+            border-radius: 10px;
+            background-repeat: no-repeat;
+            background-position: center;
+            font-size: 16px;
+            text-decoration: none;
+            color: #fff
+        }
+
+        .add:hover {
+            background-color: #589c0f;
+        }
+        
+        .edit {
+            background-image: url("images/edit.svg");
+            background-color: #121F80;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        .edit:hover {
+            background-color: #0F459C;
+        }
+
+        .delete {
+            background-image: url("images/delete.png");
+            background-color: #D72929;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        .delete:hover {
+            background-color: #e44242;
+        }
+
+    </style>
+</html>
